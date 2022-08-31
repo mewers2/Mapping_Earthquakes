243164f43b1d83a06be69fa6003f7ec7d0ac86a9
lmz17 <106768362+mewers2@users.noreply.github.com>
2022-08-31 14:09:52 -0400

    Adding updated code files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -58,6 +58,30 @@ dark.addTo(map);
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // Add a circle (with 100 meter radius) to the map for Los Angeles, California.
-circle = L.circleMarker([34.0522, -118.2437], {
-    radius: 300, color: 'black', fillColor: '#ffffa1'
-}).addTo(map);
+// circle = L.circleMarker([34.0522, -118.2437], {
+//     radius: 300, color: 'black', fillColor: '#ffffa1'
+// }).addTo(map);
+
+// Get data from cities.js array called cities
+let cityData = cities;
+
+// // Add multiple markers to the map for the array of cities.
+// cityData.forEach(function(city){
+//     console.log(city)
+//     L.marker(city.location)
+//     // Add a popup using `bindPopup()`
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Make the markers circles the size of the city population with the circleMarker() method
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: (city.population-200000)/100000, weight: 4, color: '#ffd0a1', fillColor: '#ffd0a1'
+    })
+    // Add a popup using `bindPopup()`
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
