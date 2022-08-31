4792595dd2cce159521b62e21478eae4dfaa3c77
lmz17 <106768362+mewers2@users.noreply.github.com>
2022-08-31 12:47:02 -0400

    Adding Single Point Map files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,8 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-var map = L.map('mapid').setView([40.7, -94.5], 4);
+//var map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Line 5 does this:
 // 1. We're assigning the variable map to the object `L.map()`, and we'll instantiate 
@@ -40,5 +41,23 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     accessToken: API_KEY
 
 });
+
+// Create the dark tile layer //styles/mapbox/dark-v10)
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+
+});
+
 // add the 'streets' tile layer to the map.
-streets.addTo(map);
+//streets.addTo(map);
+dark.addTo(map);
+
+// // Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle (with 100 meter radius) to the map for Los Angeles, California.
+circle = L.circleMarker([34.0522, -118.2437], {
+    radius: 300, color: 'black', fillColor: '#ffffa1'
+}).addTo(map);
