d2d7186cd30d4bfddae67486d0aafd64d6240100
lmz17 <106768362+mewers2@users.noreply.github.com>
2022-08-31 14:27:19 -0400

    Adding initial Mapping_Lines files and folders

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,87 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+var map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Line 5 does this:
+// 1. We're assigning the variable map to the object `L.map()`, and we'll instantiate 
+//the object with the given string `'mapid'`.
+// 2. The `mapid` will reference the `id` tag in our `<div>` element on the `index.html` file.
+// 3. The `setView()` method sets the view of the map with a geographical center, 
+//where the first coordinate is latitude (40.7) and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.
+
+// An alternate to the `setView!)` method that is useful when we need to add multiple tile layers or a background image of our map(s):
+// Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+
+
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: API_KEY
+// });
+// // Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// Create the tile layer that will be the background of the map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+
+});
+
+// Create the dark tile layer //styles/mapbox/dark-v10)
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+
+});
+
+// add the 'streets' tile layer to the map.
+//streets.addTo(map);
+dark.addTo(map);
+
+// // Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle (with 100 meter radius) to the map for Los Angeles, California.
+// circle = L.circleMarker([34.0522, -118.2437], {
+//     radius: 300, color: 'black', fillColor: '#ffffa1'
+// }).addTo(map);
+
+// Get data from cities.js array called cities
+let cityData = cities;
+
+// // Add multiple markers to the map for the array of cities.
+// cityData.forEach(function(city){
+//     console.log(city)
+//     L.marker(city.location)
+//     // Add a popup using `bindPopup()`
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Make the markers circles the size of the city population with the circleMarker() method
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: (city.population-200000)/100000, weight: 4, color: '#ffd0a1', fillColor: '#ffd0a1'
+    })
+    // Add a popup using `bindPopup()`
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
